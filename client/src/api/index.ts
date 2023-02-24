1324c0b4b152ad266bd6a9e751d52267cc14d1fe
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-24 10:10:00 +0900

    change url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 const devURL = "http://localhost:5001";
 const prodURL = "http://13.112.212.213";
 
-const API = axios.create({ baseURL: devURL });
+const API = axios.create({ baseURL: prodURL });
 
 export default API;
